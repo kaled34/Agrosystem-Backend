4b3e8fe5dcd3489ebbde3a30d87b38098fba4c4e
kaled34 <243759@ids.upchiapas.edu.mx>
2025-11-14 11:27:48 -0600

    con env arrglado

--- src/main/java/Config/ConfigDB.java
+++ src/main/java/Config/ConfigDB.java
@@ -10,11 +10,11 @@ public class ConfigDB {
 
     public static DataSource getDataSource() {
         if (dataSource == null) {
-            String host = "127.0.0.1";
-            String port = "3306";
-            String dbName = "agrova";
-            String username = "root";
-            String password = "root";
+            String host = "DB_HOST";
+            String port = "DB_PORT";
+            String dbName = "DB_NAME";
+            String username = "DB_USER";
+            String password = "DB_PASS";
             String url = "jdbc:mysql://" + host + ":" + port + "/" + dbName + "?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
 
             HikariConfig config = new HikariConfig();
